a8f51e81163f9126d32a2bc0aeda9d3fa54586ba
lngohumble <lngohumble@gmail.com>
2021-09-17 21:25:34 +0200

    rename

--- handler.go
+++ handler.go
@@ -8,7 +8,7 @@ import (
 )
 
 type Handler struct {
-	Endpoint    []interface{}
+	Endpoints   []interface{}
 	Handler     interface{}
 	Interceptor *Interceptor
 }
@@ -18,7 +18,7 @@ func (bot TipBot) registerTelegramHandlers() {
 	telegramHandlerRegistration.Do(func() {
 		// Set up handlers
 		for _, h := range bot.getHandler() {
-			fmt.Println("registering", h.Endpoint)
+			fmt.Println("registering", h.Endpoints)
 			bot.register(h)
 		}
 
@@ -28,7 +28,7 @@ func (bot TipBot) registerTelegramHandlers() {
 func (bot TipBot) registerHandlerWithInterceptor(h Handler) {
 	switch h.Interceptor.Type {
 	case MessageInterceptor:
-		for _, endpoint := range h.Endpoint {
+		for _, endpoint := range h.Endpoints {
 			bot.telegram.Handle(endpoint,
 				intercept.HandlerWithMessage(h.Handler.(func(ctx context.Context, query *tb.Message)),
 					intercept.WithBeforeMessage(h.Interceptor.BeforeMessage...),
@@ -36,14 +36,14 @@ func (bot TipBot) registerHandlerWithInterceptor(h Handler) {
 		}
 
 	case QueryInterceptor:
-		for _, endpoint := range h.Endpoint {
+		for _, endpoint := range h.Endpoints {
 			bot.telegram.Handle(endpoint,
 				intercept.HandlerWithQuery(h.Handler.(func(ctx context.Context, query *tb.Query)),
 					intercept.WithBeforeQuery(h.Interceptor.BeforeQuery...),
 					intercept.WithAfterQuery(h.Interceptor.AfterQuery...)))
 		}
 	case CallbackInterceptor:
-		for _, endpoint := range h.Endpoint {
+		for _, endpoint := range h.Endpoints {
 			bot.telegram.Handle(endpoint,
 				intercept.HandlerWithCallback(h.Handler.(func(ctx context.Context, callback *tb.Callback)),
 					intercept.WithBeforeCallback(h.Interceptor.BeforeCallback...),
@@ -55,7 +55,7 @@ func (bot TipBot) register(h Handler) {
 	if h.Interceptor != nil {
 		bot.registerHandlerWithInterceptor(h)
 	} else {
-		for _, endpoint := range h.Endpoint {
+		for _, endpoint := range h.Endpoints {
 			bot.telegram.Handle(endpoint, h.Handler)
 		}
 	}
@@ -63,134 +63,134 @@ func (bot TipBot) register(h Handler) {
 func (bot TipBot) getHandler() []Handler {
 	return []Handler{
 		{
-			Endpoint: []interface{}{"/start"},
-			Handler:  bot.startHandler,
+			Endpoints: []interface{}{"/start"},
+			Handler:   bot.startHandler,
 		},
 		{
-			Endpoint:    []interface{}{"/faucet", "/zapfhahn", "/kraan"},
+			Endpoints:   []interface{}{"/faucet", "/zapfhahn", "/kraan"},
 			Handler:     bot.faucetHandler,
 			Interceptor: &Interceptor{Type: MessageInterceptor, BeforeMessage: []intercept.MessageFunc{bot.loadUserInterceptor}},
 		},
 		{
-			Endpoint:    []interface{}{"/tip"},
+			Endpoints:   []interface{}{"/tip"},
 			Handler:     bot.tipHandler,
 			Interceptor: &Interceptor{Type: MessageInterceptor, BeforeMessage: []intercept.MessageFunc{bot.loadUserInterceptor, bot.loadReplyToInterceptor}},
 		},
 		{
-			Endpoint:    []interface{}{"/pay"},
+			Endpoints:   []interface{}{"/pay"},
 			Handler:     bot.confirmPaymentHandler,
 			Interceptor: &Interceptor{Type: MessageInterceptor, BeforeMessage: []intercept.MessageFunc{bot.loadUserInterceptor}},
 		},
 		{
-			Endpoint:    []interface{}{"/invoice"},
+			Endpoints:   []interface{}{"/invoice"},
 			Handler:     bot.invoiceHandler,
 			Interceptor: &Interceptor{Type: MessageInterceptor, BeforeMessage: []intercept.MessageFunc{bot.loadUserInterceptor}},
 		},
 		{
-			Endpoint:    []interface{}{"/balance"},
+			Endpoints:   []interface{}{"/balance"},
 			Handler:     bot.balanceHandler,
 			Interceptor: &Interceptor{Type: MessageInterceptor, BeforeMessage: []intercept.MessageFunc{bot.loadUserInterceptor}},
 		},
 		{
-			Endpoint:    []interface{}{"/send"},
+			Endpoints:   []interface{}{"/send"},
 			Handler:     bot.confirmSendHandler,
 			Interceptor: &Interceptor{Type: MessageInterceptor, BeforeMessage: []intercept.MessageFunc{bot.loadUserInterceptor}},
 		},
 		{
-			Endpoint:    []interface{}{"/help"},
+			Endpoints:   []interface{}{"/help"},
 			Handler:     bot.helpHandler,
 			Interceptor: &Interceptor{Type: MessageInterceptor, BeforeMessage: []intercept.MessageFunc{bot.loadUserInterceptor}},
 		},
 		{
-			Endpoint:    []interface{}{"/basics"},
+			Endpoints:   []interface{}{"/basics"},
 			Handler:     bot.basicsHandler,
 			Interceptor: &Interceptor{Type: MessageInterceptor, BeforeMessage: []intercept.MessageFunc{bot.loadUserInterceptor}},
 		},
 		{
-			Endpoint:    []interface{}{"/donate"},
+			Endpoints:   []interface{}{"/donate"},
 			Handler:     bot.donationHandler,
 			Interceptor: &Interceptor{Type: MessageInterceptor, BeforeMessage: []intercept.MessageFunc{bot.loadUserInterceptor}},
 		},
 		{
-			Endpoint:    []interface{}{"/advanced"},
+			Endpoints:   []interface{}{"/advanced"},
 			Handler:     bot.advancedHelpHandler,
 			Interceptor: &Interceptor{Type: MessageInterceptor, BeforeMessage: []intercept.MessageFunc{bot.loadUserInterceptor}},
 		},
 		{
-			Endpoint:    []interface{}{"/link"},
+			Endpoints:   []interface{}{"/link"},
 			Handler:     bot.lndhubHandler,
 			Interceptor: &Interceptor{Type: MessageInterceptor, BeforeMessage: []intercept.MessageFunc{bot.loadUserInterceptor}},
 		},
 		{
-			Endpoint:    []interface{}{"/lnurl"},
+			Endpoints:   []interface{}{"/lnurl"},
 			Handler:     bot.lnurlHandler,
 			Interceptor: &Interceptor{Type: MessageInterceptor, BeforeMessage: []intercept.MessageFunc{bot.loadUserInterceptor}},
 		},
 		{
-			Endpoint:    []interface{}{tb.OnPhoto},
+			Endpoints:   []interface{}{tb.OnPhoto},
 			Handler:     bot.privatePhotoHandler,
 			Interceptor: &Interceptor{Type: MessageInterceptor, BeforeMessage: []intercept.MessageFunc{bot.loadUserInterceptor}},
 		},
 		{
-			Endpoint:    []interface{}{tb.OnText},
+			Endpoints:   []interface{}{tb.OnText},
 			Handler:     bot.anyTextHandler,
 			Interceptor: &Interceptor{Type: MessageInterceptor, BeforeMessage: []intercept.MessageFunc{bot.loadUserInterceptor}},
 		},
 		{
-			Endpoint:    []interface{}{tb.OnQuery},
+			Endpoints:   []interface{}{tb.OnQuery},
 			Handler:     bot.anyQueryHandler,
 			Interceptor: &Interceptor{Type: QueryInterceptor, BeforeQuery: []intercept.QueryFunc{bot.loadUserQueryInterceptor}},
 		},
 		{
-			Endpoint: []interface{}{tb.OnChosenInlineResult},
-			Handler:  bot.anyChosenInlineHandler,
+			Endpoints: []interface{}{tb.OnChosenInlineResult},
+			Handler:   bot.anyChosenInlineHandler,
 		},
 		{
-			Endpoint:    []interface{}{&btnPay},
+			Endpoints:   []interface{}{&btnPay},
 			Handler:     bot.payHandler,
 			Interceptor: &Interceptor{Type: CallbackInterceptor, BeforeCallback: []intercept.CallbackFunc{bot.loadUserCallbackInterceptor}},
 		},
 		{
-			Endpoint:    []interface{}{&btnCancelPay},
+			Endpoints:   []interface{}{&btnCancelPay},
 			Handler:     bot.cancelPaymentHandler,
 			Interceptor: &Interceptor{Type: CallbackInterceptor, BeforeCallback: []intercept.CallbackFunc{bot.loadUserCallbackInterceptor}},
 		},
 		{
-			Endpoint:    []interface{}{&btnSend},
+			Endpoints:   []interface{}{&btnSend},
 			Handler:     bot.sendHandler,
 			Interceptor: &Interceptor{Type: CallbackInterceptor, BeforeCallback: []intercept.CallbackFunc{bot.loadUserCallbackInterceptor}},
 		},
 		{
-			Endpoint:    []interface{}{&btnCancelSend},
+			Endpoints:   []interface{}{&btnCancelSend},
 			Handler:     bot.cancelSendHandler,
 			Interceptor: &Interceptor{Type: CallbackInterceptor, BeforeCallback: []intercept.CallbackFunc{bot.loadUserCallbackInterceptor}},
 		},
 		{
-			Endpoint:    []interface{}{&btnAcceptInlineSend},
+			Endpoints:   []interface{}{&btnAcceptInlineSend},
 			Handler:     bot.acceptInlineSendHandler,
 			Interceptor: &Interceptor{Type: CallbackInterceptor, BeforeCallback: []intercept.CallbackFunc{bot.loadUserCallbackInterceptor}},
 		},
 		{
-			Endpoint: []interface{}{&btnCancelInlineSend},
-			Handler:  bot.cancelInlineSendHandler,
+			Endpoints: []interface{}{&btnCancelInlineSend},
+			Handler:   bot.cancelInlineSendHandler,
 		},
 		{
-			Endpoint:    []interface{}{&btnAcceptInlineReceive},
+			Endpoints:   []interface{}{&btnAcceptInlineReceive},
 			Handler:     bot.acceptInlineReceiveHandler,
 			Interceptor: &Interceptor{Type: CallbackInterceptor, BeforeCallback: []intercept.CallbackFunc{bot.loadUserCallbackInterceptor}},
 		},
 		{
-			Endpoint: []interface{}{&btnCancelInlineReceive},
-			Handler:  bot.cancelInlineReceiveHandler,
+			Endpoints: []interface{}{&btnCancelInlineReceive},
+			Handler:   bot.cancelInlineReceiveHandler,
 		},
 		{
-			Endpoint:    []interface{}{&btnAcceptInlineFaucet},
+			Endpoints:   []interface{}{&btnAcceptInlineFaucet},
 			Handler:     bot.accpetInlineFaucetHandler,
 			Interceptor: &Interceptor{Type: CallbackInterceptor, BeforeCallback: []intercept.CallbackFunc{bot.loadUserCallbackInterceptor}},
 		},
 		{
-			Endpoint: []interface{}{&btnCancelInlineFaucet},
-			Handler:  bot.cancelInlineFaucetHandler,
+			Endpoints: []interface{}{&btnCancelInlineFaucet},
+			Handler:   bot.cancelInlineFaucetHandler,
 		},
 	}
 }
